8384d5e16a5d695c670524a0999edbeb931df9fa
wyang19a <wyang19a@gmail.com>
2020-10-23 22:40:52 +0900

    Implements Sanity.io

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
+// import dotenv from 'dotenv';
+
+// dotenv.config({ path: '.env' });
+
 module.exports = {
   siteMetadata: {
     title: `Wootae`,
     description: `Portfolio to showcase my work`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-transition-link`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'qdbd2apt',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import projects from '../components/ProjectList';
 
 const PortfolioStyles = styled.div`
   padding: 5.5rem;
@@ -14,16 +13,18 @@ const SingleProjectStyle = styled.div`
 const SingleProject = ({ project }) => (
   <SingleProjectStyle>
     <h2>{project.name}</h2>
-    <img src={project.imgPath} alt={project.name} />
+    {/* <img src={project.imgPath} alt={project.name} /> */}
     {/* <img src="../assets/images/comp.png" alt="Hi" /> */}
   </SingleProjectStyle>
 );
 const PortfolioPage = () => (
   <PortfolioStyles>
-    {projects.map((project) => (
+    <p>Hi</p>
+    {/* {projects.map((project) => (
       <SingleProject key={project.id} project={project} />
-    ))}
+    ))} */}
   </PortfolioStyles>
 );
 
 export default PortfolioPage;
+
