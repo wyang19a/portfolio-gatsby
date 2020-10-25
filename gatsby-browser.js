f9658f1b645670d522cbe9887a3894a73b2045ba
wyang19a <wyang19a@gmail.com>
2020-10-25 22:54:55 +0900

    Fixes Layout path. to be fixed to Uppercase later

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './src/components/Layout';
+import Layout from './src/components/layout';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
