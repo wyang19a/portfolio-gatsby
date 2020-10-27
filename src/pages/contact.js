8cfbe72331750da29e83be1d065274d115be59af
wyang19a <wyang19a@gmail.com>
2020-10-27 18:09:03 +0900

    Adds Email and Phone

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import {
+  AiOutlineMail as MailIcon,
+  AiOutlinePhone as PhoneIcon,
+} from 'react-icons/ai';
 
 const ContactStyles = styled.div`
+  display: grid;
   padding: 5.5rem;
+  text-align: center;
+  justify-content: center;
+  form {
+    display: grid;
+    gap: 1rem;
+    width: 500px;
+  }
 `;
 
 const ContactPage = () => {
   const [state, setState] = useState('');
   const [ggul, setGgul] = useState('');
-  const { status } = state;
   const submitForm = (ev) => {
     ev.preventDefault();
     const form = ev.target;
@@ -34,47 +45,60 @@ const ContactPage = () => {
   };
   return (
     <ContactStyles>
-      <form
-        method="post"
-        onSubmit={submitForm}
-        action="https://formspree.io/mnqvgjad"
-      >
-        <input
-          type="email"
-          placeholder="email *"
-          name="email"
-          id="email"
-          required="required"
-        />
-        <input placeholder="name" type="text" name="name" id="name" />
-        <input placeholder="subject" type="text" name="subject" id="subject" />
-        <textarea
-          name="message"
-          placeholder="message *"
-          id="message"
-          rows="5"
-          required="required"
-        />
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-        <input
-          type="ggul"
-          name="ggul"
-          id="ggul"
-          className="ggul"
-          onChange={updateGgul}
-        />
-        {state === 'SUCCESS' ? (
-          <p>Thanks! I'll get back to you as soon as I can!</p>
-        ) : (
-          ''
-        )}
-        {state === 'ERROR' && (
-          <p>
-            Oops! There was an error. Please make sure your email is correct.
-          </p>
-        )}
-      </form>
+      <div className="item">
+        <MailIcon />
+        wyang19a@gmail.com
+      </div>
+      <div className="item">
+        <PhoneIcon />
+        617-990-6178
+      </div>
+      <div className="item">
+        <form
+          method="post"
+          onSubmit={submitForm}
+          action="https://formspree.io/mnqvgjad"
+        >
+          <input
+            type="email"
+            placeholder="email *"
+            name="email"
+            id="email"
+            required="required"
+          />
+          <input placeholder="name" type="text" name="name" id="name" />
+          <input
+            placeholder="subject"
+            type="text"
+            name="subject"
+            id="subject"
+          />
+          <textarea
+            name="message"
+            placeholder="message *"
+            id="message"
+            rows="5"
+            required="required"
+          />
+          <button type="submit">Send</button>
+          <input type="reset" value="Clear" />
+          <input
+            type="ggul"
+            name="ggul"
+            id="ggul"
+            className="ggul"
+            onChange={updateGgul}
+          />
+          {state === 'SUCCESS' ? (
+            <p>Thank you. I'll get back to you as soon as I can!</p>
+          ) : (
+            ''
+          )}
+          {state === 'ERROR' && (
+            <p>Oops! There was an error. Please double check your email.</p>
+          )}
+        </form>
+      </div>
     </ContactStyles>
   );
 };
