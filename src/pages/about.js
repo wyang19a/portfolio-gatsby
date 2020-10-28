fbea096716f626f9adb59fa69c0be69cfaf83401
wyang19a <wyang19a@gmail.com>
2020-10-28 18:15:08 +0900

    Adds resume and skills

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,26 @@
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import Resume from '../static/resume.pdf';
 
 const AboutStyles = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-end;
-  padding: 5.5rem;
+  align-items: center;
+  padding: 4rem;
   h1.name {
     font-size: 3rem;
   }
   span.title {
     font-size: 1.5rem;
   }
-  p.sub-name {
-    font-size: 1rem;
-    /* text-align: left; */
-  }
+
   p.description {
     font-size: 0.8rem;
     width: 75%;
-    text-align: right;
+    text-align: center;
   }
   .gatsby-image-wrapper {
     width: 150px;
@@ -30,19 +28,68 @@ const AboutStyles = styled.div`
     border: 2px solid white;
     margin-bottom: 10px;
   }
+  .logos {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    background: grey;
+    padding: 20px;
+    /* border-radius: 10px; */
+    .gatsby-image-wrapper {
+      width: 100px;
+      border-radius: 0;
+      border: 0;
+    }
+  }
+  .logo {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    grid-template-rows: auto 30px;
+    img {
+      justify-content: space-around;
+      align-items: center;
+    }
+    p {
+      text-align: center;
+    }
+  }
+  .resume-btn {
+    color: white;
+    padding: 4px;
+    margin: 4px;
+    background: #0f3959;
+    border: 1px solid;
+  }
 `;
 
-const AboutPage = ({ data: { profile } }) => {
-  console.log(profile);
+const SingleLogo = ({ logo }) => (
+  <div className="logo">
+    <Img fluid={logo.image.asset.fluid} alt={logo.name} />
+    <p>{logo.name}</p>
+  </div>
+);
+const AboutPage = ({ data: { profile, logos } }) => {
+  const logoList = logos.nodes;
+  console.log(logoList);
   return (
-    <AboutStyles>
-      <Img fluid={profile.image.asset.fluid} alt={profile.name} />
-      <h1 className="name">
-        {profile.name}, <span className="title">{profile.title}</span>
-      </h1>
-      <p className="sub-name">{profile.position}</p>
-      <p className="description">{profile.description}</p>
-    </AboutStyles>
+    <>
+      <AboutStyles>
+        <Img fluid={profile.image.asset.fluid} alt={profile.name} />
+        <Link href={Resume} className="resume-btn">
+          View Resume
+        </Link>
+        <h1 className="name">
+          {profile.name}, <span className="title">{profile.title}</span>
+        </h1>
+        <p className="sub-name">{profile.position}</p>
+        <p className="description">{profile.description}</p>
+        <div className="logos">
+          {logoList.map((logo) => (
+            <SingleLogo key={logo.id} logo={logo} />
+          ))}
+        </div>
+      </AboutStyles>
+    </>
   );
 };
 
@@ -57,6 +104,18 @@ export const query = graphql`
         asset {
           fluid(maxWidth: 500) {
             ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+    logos: allSanityLogos {
+      nodes {
+        name
+        image {
+          asset {
+            fluid(maxWidth: 150) {
+              ...GatsbySanityImageFluid
+            }
           }
         }
       }
