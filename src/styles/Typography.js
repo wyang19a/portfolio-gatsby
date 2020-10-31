617fbce3fdaec315106ab7f7b06fc39cc2787f66
wyang19a <wyang19a@gmail.com>
2020-10-31 22:31:28 +0900

    Removes font-face. To be added later

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,22 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const Typography = createGlobalStyle`
-@font-face {
-  font-family: 'Playfair Display';
-  font-style: normal;
-  font-weight: 400;
-  src: url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
-  /* font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 400;
-  src: url('../fonts/open-sans-v18-latin-regular.eot'); IE9 Compat Modes
-  src: local('Open Sans Regular'), local('OpenSans-Regular'),
-       url('../fonts/open-sans-v18-latin-regular.eot?#iefix') format('embedded-opentype'), IE6-IE8
-       url('../fonts/open-sans-v18-latin-regular.woff2') format('woff2'), Super Modern Browsers
-       url('../fonts/open-sans-v18-latin-regular.woff') format('woff'), Modern Browsers
-       url('../fonts/open-sans-v18-latin-regular.ttf') format('truetype'), Safari, Android, iOS
-       url('../fonts/open-sans-v18-latin-regular.svg#OpenSans') format('svg'); Legacy iOS */
-}
+
 
   html {
     font-family: Playfair Display, 'Open Sans', 'Helvetica Neue', sans-serif;
