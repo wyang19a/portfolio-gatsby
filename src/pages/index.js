00e29cb58f305a98774a7ac7fcd600951380f054
wyang19a <wyang19a@gmail.com>
2020-10-31 22:28:34 +0900

    Edits home page. Adds Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
+import Typed from 'react-typed';
+import { motion } from 'framer-motion';
+import HomeImage from '../static/homeimage.png';
 
-const HomeStyles = styled.div`
-  padding: 5.5rem;
-  p {
-    position: absolute;
-    color: transparent;
-    letter-spacing: 5px;
-  }
-  p:before {
-    content: attr(data-text);
-    position: absolute;
-    top: 0;
-    color: white;
-    overflow: hidden;
-    border-right: 1px solid white;
-    animation: type 5s steps(56) 1;
-    white-space: nowrap;
-  }
-  @keyframes type {
-    0% {
-      width: 0;
-    }
-    100% {
-      width: 100%;
-    }
+const HomeGridStyle = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  .Img {
+    align-self: center;
+
+    width: 28vw;
+    /* background-color: red; */
+    filter: invert(0.7);
   }
 `;
 
-const IndexPage = () => (
-  <HomeStyles>
-    <h1>WELCOME</h1>
-    <p data-text="Fullstack developer based in Boston, MA">
-      Fullstack developer based in Boston, MA
-    </p>
-  </HomeStyles>
-);
+const HomeStyles = styled.div`
+  padding: 5.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 11rem);
+  transform: translateY(-60px);
+  .typedString {
+    font-size: 1.2rem;
+    width: 75%;
+    position: absolute !important;
+    text-transform: uppercase;
+    span:nth-child(1) {
+      letter-spacing: 3px;
+    }
+  }
+  h1 {
+    font-size: 4rem;
+    padding-bottom: 40px;
+  }
+`;
+
+const IndexPage = ({ data: { intro } }) => {
+  console.log(intro);
+  return (
+    <HomeGridStyle>
+      <HomeStyles>
+        <h1>
+          Hi! <br />
+          I'm Wootae,
+        </h1>
+        <div>
+          <Typed
+            className="typedString"
+            strings={[
+              'Fullstack Engineer / SQL Database Developer Based In Boston, MA.',
+            ]}
+            typeSpeed={40}
+          />
+        </div>
+      </HomeStyles>
+      <motion.div
+        className="Img"
+        initial={{ x: 3000 }}
+        animate={{ x: 220 }}
+        transition={{ delay: 3.5, type: 'tween', duration: 1 }}
+      >
+        <img src={HomeImage} alt="Homeimg" />
+      </motion.div>
+    </HomeGridStyle>
+  );
+};
 
 export default IndexPage;
