82249843c449d4bcc567417dd2b0844d4e5a7ec7
wyang19a <wyang19a@gmail.com>
2020-10-31 23:57:49 +0900

    Changes Layout import to Layout.js import

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './src/components/Layout';
+import Layout from './src/components/Layout.js';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
